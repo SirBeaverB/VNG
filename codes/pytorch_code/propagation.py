e318625c036cd827500b48d28471273bbdecd5db
Jialing <bquantumwill@gmail.com>
2024-11-10 01:15:05 -0800

    matrics multiply

--- codes/pytorch_code/propagation.py
+++ codes/pytorch_code/propagation.py
@@ -186,19 +186,34 @@ def vng_track_pi(new_adj_matrix: sp.spmatrix, old_adj_matrix: sp.spmatrix, alpha
     #calculate P
     P = vng_compute_P(alpha, M_prime, r) #r should be n*1
 
-    P11 = P[:g, :g] # g*g
+    n = P.shape[0]
+
+    """P11 = P[:g, :g] # g*g
     P12 = P[:g, g:] # g*(n-g)
     P21 = P[g:, :g] # (n-g)*g
-    P22 = P[g:, g:] # (n-g)*(n-g)
+    P22 = P[g:, g:] # (n-g)*(n-g)"""
 
     # initialize s_T
     theta = r[g:] # (n-g)*1
-    e = np.ones((theta.shape[0], 1))  # (n-g)*1
+    e = np.ones((n-g, 1))  # (n-g)*1
     s_T = theta.T/(theta.T @ e) # 1*(n-g)
+    I = np.eye(g)
+
+    rows_I, cols_I = g, g
+    rows_e, cols_e = e.shape
+    rows_s_T, cols_s_T = s_T.shape
+
+    E = np.zeros((rows_I + rows_e, cols_I + cols_e))
+    E[:rows_I, :cols_I] = I
+    E[rows_I:, cols_I:] = e
+
+    S = np.zeros((rows_I + rows_s_T, cols_I + cols_s_T))
+    S[:rows_I, :cols_I] = I
+    S[rows_I:, cols_I:] = s_T
 
     for _ in range(100):
         # step 2
-        U11 = P11 # g*g
+        """U11 = P11 # g*g
 
         e = np.ones((P12.shape[1], 1))  # (n-g)*1
         U12 = P12 @ e
@@ -212,8 +227,10 @@ def vng_track_pi(new_adj_matrix: sp.spmatrix, old_adj_matrix: sp.spmatrix, alpha
 
         top = np.hstack((U11, U12))
         bottom = np.hstack((U21, U22))
-        U = np.vstack((top, bottom)) # (g+1)*(g+1)
+        U = np.vstack((top, bottom)) # (g+1)*(g+1)"""
+        
 
+        U = S @ P @ E
         
         # step 3
         phi_T = vng_power_method(U) # (g+1)*1
@@ -229,8 +246,12 @@ def vng_track_pi(new_adj_matrix: sp.spmatrix, old_adj_matrix: sp.spmatrix, alpha
         if np.linalg.norm(pi_hat_T - pi, 1) < 0.0001:
             break
 
-        theta = pi_hat_T[:, g:] # (n-g)*1
-        s_T = theta.T/(theta @ e) # 1*(n-g)
+        theta = pi_hat_T[:, g:] # 1*(n-g)
+        s_T = theta/(theta @ e) # 1*(n-g)
+
+        S = np.zeros((rows_I + rows_s_T, cols_I + cols_s_T))
+        S[:rows_I, :cols_I] = I
+        S[rows_I:, cols_I:] = s_T
 
     return pi
 
